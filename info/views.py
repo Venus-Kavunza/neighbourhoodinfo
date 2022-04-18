30be75e39a8820afc16577fd134e9ef82b7a36f0
Venus-Kavunza <venusmwende@gmail.com>
2022-04-18 20:52:09 +0300

    it will add the form for updating the view function for the post

--- info/views.py
+++ info/views.py
@@ -24,3 +24,109 @@ def register(request):
     else:
         form = SignUpForm()
     return render(request, 'registration/registration_form.html', {'form': form})
+
+@login_required(login_url='/accounts/login/')
+def hood(request):
+    hoods = Neighbourhood.objects.all()
+    return render(request, 'neighbourhoods.html', {"hoods": hoods})
+
+
+@login_required(login_url='/accounts/login/')
+def new_hood(request):
+    current_user = request.user
+    if request.method == 'POST':
+        form = NewHoodForm(request.POST, request.FILES)
+        if form.is_valid():
+            image = form.save(commit=False)
+            image.admin = current_user.profile
+
+            image.save()
+
+        return redirect('hood')
+
+    else:
+        form = NewHoodForm()
+    return render(request, 'new_hood.html', {"form": form})
+
+
+def edit_hood(request):
+    current_user = request.user
+    if request.method == 'POST':
+        form = EditHoodForm(request.POST, request.FILES, instance=request.user)
+        if form.is_valid():
+            image = form.save(commit=False)
+            image.admin = current_user.profile
+
+            image.save()
+        return redirect('hood')
+
+    else:
+        form = EditHoodForm()
+    return render(request, 'edit_hood.html', {'form': form})
+
+
+def joinhood(request, id):
+    hood = get_object_or_404(Neighbourhood, id=id)
+    request.user.profile.neighbourhood = hood
+    request.user.profile.save()
+    return redirect('hood')
+
+
+def leavehood(request, id):
+    hood = get_object_or_404(Neighbourhood, id=id)
+    request.user.profile.neighbourhood = None
+    request.user.profile.save()
+    return redirect('hood')
+
+
+@login_required(login_url='/accounts/login/')
+def singlehood(request, id):
+    hood = Neighbourhood.objects.get(id=id)
+    return render(request, 'hood.html', {'hood':hood})
+
+@login_required(login_url='/accounts/login/')
+def businesses(request, id):
+    business = Business. hood_biz(id=id)
+    return render(request, 'business.html', {'business': business})
+
+
+@login_required(login_url='/accounts/login/')
+def newbiz(request):
+    current_user = request.user
+    if request.method == 'POST':
+        form = NewBizForm(request.POST, request.FILES)
+        if form.is_valid():
+            business = form.save(commit=False)
+            business.user = current_user
+
+            business.save()
+
+        return redirect('hood')
+
+    else:
+        form = NewBizForm()
+    return render(request, 'new_business.html', {"form": form})
+
+
+@login_required(login_url='/accounts/login/')
+def posthood(request, id):
+    post = Post.hood_post(id=id)
+    return render(request, 'hoodpost.html', {'post': post})
+
+
+@login_required(login_url='/accounts/login/')
+def post(request):
+    current_user = request.user
+    if request.method == 'POST':
+        form = NewPostForm(request.POST, request.FILES)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.user = current_user
+
+            post.save()
+
+        return redirect('hood')
+
+    else:
+        form = NewPostForm()
+    return render(request, 'post.html', {"form": form})
